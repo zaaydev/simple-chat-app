a4caab5a4ab1b3aebae2c5db429f4f2970380fa9
zaaydev <xaayrole@gmail.com>
2026-01-08 15:05:52 +0530

    Fixed Cors Issue

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -122,7 +122,7 @@ export const handleLogIn = async (req, res) => {
     res.cookie("jwt_cookie", token, {
       maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days
       httpOnly: true, // prevent access via JS
-      sameSite: true, // protection against CSRF
+      sameSite: "none", // protection against CSRF
       secure: process.env.SERVER_ENV !== "development", // secure in production
     });
 
